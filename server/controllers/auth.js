0c0ea39d8dd334279d991d281124fa295729b3ee
aakashbathla <aakash.bathla7@gmail.com>
2023-02-20 14:11:41 +0400

    code updated

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,59 +1,67 @@
 import db from "../db.js";
-import bcrypt from "bcrypt"
-import Jwt from "jsonwebtoken"
+import bcrypt from "bcrypt";
+import Jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
-    try {
-        db.connect()
-        const q = "SELECT * FROM users WHERE email=? OR user_name=?"
-        db.query(q, [req.body.email, req.body.username], async (error, data) => {
-            if (error) return res.json(error)
-            if (data.length) return res.status(409).json("User already exists")
-            // Hash the password
-            const salt = bcrypt.genSaltSync(10);
-            const hash = bcrypt.hashSync(req.body.password, salt);
-            const q = "INSERT INTO users (user_name,email,password,user_type) VALUES (?) "
-            const values = [
-                req.body.username,
-                req.body.email,
-                hash,
-                'user'
-            ]
-            db.query(q, [values], (err, data) => {
-                if (err) return res.json(err)
-                return res.status(200).json("User hash been created")
-            })
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    db.connect();
+    const q = "SELECT * FROM users WHERE email=? OR user_name=?";
+    db.query(q, [req.body.email, req.body.username], async (error, data) => {
+      if (error) return res.json(error);
+      if (data.length) return res.status(409).json("User already exists");
+      // Hash the password
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(req.body.password, salt);
+      const q =
+        "INSERT INTO users (user_name,email,password,user_type) VALUES (?) ";
+      const values = [req.body.username, req.body.email, hash, "user"];
+      db.query(q, [values], (err, data) => {
+        if (err) return res.json(err);
+        return res.status(200).json("User hash been created");
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const login = (req, res) => {
-    try {
-        db.connect()
-        const q = "SELECT * FROM users WHERE email=?"
-        db.query(q, [req.body.email], (err, data) => {
-            if (err) return res.json(err)
-            if (data.length === 0) return res.status(404).json("User not found")
-            const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);// true
-            if (!isPasswordCorrect) return res.status(400).json("Wrong email or password")
-            const token = Jwt.sign({ id: data[0].id }, "jwtkey")
-            const { password, ...other } = data[0]
-            return res.cookie("access_token", token, { httpOnly: true }).status(200).json(other)
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    db.connect();
+    const q = "SELECT * FROM users WHERE email=?";
+    db.query(q, [req.body.email], (err, data) => {
+      if (err) return res.json(err);
+      if (data.length === 0) return res.status(404).json("User not found");
+      console.log(req.body.password);
+      console.log(data);
+      const isPasswordCorrect = bcrypt.compareSync(
+        req.body.password,
+        data[0].PASSWORD
+      ); // true
+      if (!isPasswordCorrect)
+        return res.status(400).json("Wrong email or password");
+      const token = Jwt.sign({ id: data[0].id }, "jwtkey");
+      const { password, ...other } = data[0];
+      return res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(other);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const logout = (req, res) => {
-    try {
-        return res.clearCookie("access_token", {
-            sameSite: "none",
-            secure: true
-        }).status(200).json("User has been logged out.")
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    return res
+      .clearCookie("access_token", {
+        sameSite: "none",
+        secure: true,
+      })
+      .status(200)
+      .json("User has been logged out.");
+  } catch (error) {
+    console.log(error);
+  }
+};
--- server/db.js
+++ server/db.js
@@ -1,13 +1,14 @@
 import mysql, { createConnection } from "mysql2";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
-  const db= mysql.createConnection(
-    {
-        host:'localhost',
-        user:'root',
-        password:'Semsem@25',
-        database:'blog',
-        port:'3306'
-    }
-)
+console.log(process.env.DB_HOST);
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DBNAME,
+  password: process.env.DB_PASSWORD,
+});
 
-export default db
+export default db;
