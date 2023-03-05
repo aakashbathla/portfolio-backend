289928060c60b82bd0b3aedd3ca6d0b159fc03a5
aakashbathla <aakash.bathla7@gmail.com>
2023-03-05 16:30:19 +0400

    fix: add new fields in toolbar of rich text area

--- client/src/pages/Write/Write.jsx
+++ client/src/pages/Write/Write.jsx
@@ -1,101 +1,219 @@
 import axios from "axios";
 import moment from "moment";
 import React, { useState } from "react";
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 import { useLocation, useNavigate } from "react-router-dom";
 import "./Write.scss";
 
 export default function Write() {
-    const state = useLocation().state
-    const [value, setValue] = useState(state ? state.description : "");
-    const [title, setTitle] = useState(state ? state.title : "");
-    const [file, setFile] = useState(state ? state.post_img : "");
-    const [cat, setCat] = useState(state ? state.cat : "");
-    const [isPublished, setIsPublished] = useState(state ? state.isPublished:1);
+  const state = useLocation().state;
+  const modules = {
+    toolbar: [
+      [{ font: [] }],
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["bold", "italic", "underline", "strike"],
+      [{ color: [] }, { background: [] }],
+      [{ script: "sub" }, { script: "super" }],
+      ["blockquote", "code-block"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      // [{ indent:  "-1" }, { indent:  "+1" }, { align: [] }],
+      [
+        // "link",
+        "image",
+        // "video"
+      ],
+      // ["clean"],
+    ],
+  };
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
+  const [value, setValue] = useState(state ? state.description : "");
+  const [title, setTitle] = useState(state ? state.title : "");
+  const [file, setFile] = useState(state ? state.post_img : "");
+  const [cat, setCat] = useState(state ? state.cat : "");
+  const [isPublished, setIsPublished] = useState(state ? state.isPublished : 1);
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const upload = async () => {
-        try {
-            if (file) {
-                const formData = new FormData();
-                formData.append("file", file);
-                const res = await axios.post(`${process.env.REACT_APP_BASE_URL}api/upload`, formData, { withCredentials: true });
-                return res.data
-            }
-            return state?.post_img;
-        } catch (err) {
-            console.log(err);
-        }
-    };
-
-    const handleClick = async (e) => {
-        e.preventDefault()
-        const imgUrl = await upload()
-        try {
-            state ? await axios.put(`${process.env.REACT_APP_BASE_URL}api/posts/${state.id}`, {
-                title, description: value, cat, post_img: imgUrl, isPublished:isPublished
-            }, { withCredentials: true })
-                : await axios.post(`${process.env.REACT_APP_BASE_URL}api/posts/`, {
-                    title, description: value, cat, post_img: imgUrl, date: moment(Date.now()).format("YYYY-MM-DD HH:mm"),isPublished:isPublished
-                }, { withCredentials: true })
-            navigate("/")
-        } catch (error) {
-            console.log(error);
-        }
+  const upload = async () => {
+    try {
+      if (file) {
+        const formData = new FormData();
+        formData.append("file", file);
+        const res = await axios.post(
+          `${process.env.REACT_APP_BASE_URL}api/upload`,
+          formData,
+          { withCredentials: true }
+        );
+        return res.data;
+      }
+      return state?.post_img;
+    } catch (err) {
+      console.log(err);
     }
-    return (
-        <div className="write">
-            <div className="content">
-                <input type="text" value={title} placeholder="Title" onChange={e => setTitle(e.target.value)} />
-                <div className="editorContainer">
-                    <ReactQuill theme="snow" value={value} onChange={setValue} />
-                </div>
-            </div>
-            <div className="menu">
-                <div className="item">
-                    <h1>Publish</h1>
-                    <div>
-                        <input type="radio" checked={isPublished == 1} name="Published" value={1} id="published" onChange={e => setIsPublished(e.target.value)} />
-                        <label htmlFor="published"> Published</label>
-                    </div>
-                    <div>
-                        <input type="radio" checked={isPublished == 0} name="Save as draft" value={0} id="save-as" onChange={e => setIsPublished(e.target.value)} />
-                        <label htmlFor="save-as"> Save as draft</label>
-                    </div>
+  };
 
-                    <label htmlFor="file"><b>Upload Image</b> </label>
-                    {/* Need check if there is  an img or not -- if we come to edit or to create new post  */}
-                    <input type="file" id="file" onChange={e => setFile(e.target.files[0])} />
-                </div>
-                <div className="item">
-                    <h1>Category</h1>
-                    <div>
-                        <input type="radio" checked={cat.toLowerCase() === "art"} name="cat" value="Art" id="art" onChange={e => setCat(e.target.value)} />
-                        <label htmlFor="art">Art</label>
-                    </div>
-                    <div>
-                        <input type="radio" checked={cat.toLowerCase() === "food"} name="cat" value="Food" id="Food" onChange={e => setCat(e.target.value)} />
-                        <label htmlFor="Food">Food</label>
-                    </div>
-                    <div>
-                        <input type="radio" checked={cat.toLowerCase() === "technology"} name="cat" value="Technology" id="Technology" onChange={e => setCat(e.target.value)} />
-                        <label htmlFor="Technology">Technology</label>
-                    </div>
-                    <div>
-                        <input type="radio" checked={cat.toLowerCase() === "science"} name="cat" value="Science" id="Science" onChange={e => setCat(e.target.value)} />
-                        <label htmlFor="Science">Science</label>
-                    </div>
-                    <div>
-                        <input type="radio" checked={cat.toLowerCase() === "sport"} name="cat" value="Sport" id="Sport" onChange={e => setCat(e.target.value)} />
-                        <label htmlFor="Sport">Sport</label>
-                    </div>
-                </div>
-                <div className="buttons">
-                    <button onClick={handleClick}>Publish</button>
-                </div>
-            </div>
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const imgUrl = await upload();
+    try {
+      state
+        ? await axios.put(
+            `${process.env.REACT_APP_BASE_URL}api/posts/${state.id}`,
+            {
+              title,
+              description: value,
+              cat,
+              post_img: imgUrl,
+              isPublished: isPublished,
+            },
+            { withCredentials: true }
+          )
+        : await axios.post(
+            `${process.env.REACT_APP_BASE_URL}api/posts/`,
+            {
+              title,
+              description: value,
+              cat,
+              post_img: imgUrl,
+              date: moment(Date.now()).format("YYYY-MM-DD HH:mm"),
+              isPublished: isPublished,
+            },
+            { withCredentials: true }
+          );
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="write">
+      <div className="content">
+        <input
+          type="text"
+          value={title}
+          placeholder="Title"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <div className="editorContainer">
+          <ReactQuill
+            theme="snow"
+            modules={modules}
+            value={value}
+            onChange={setValue}
+          />
         </div>
-    )
+      </div>
+      <div className="menu">
+        <div className="item">
+          <h1>Publish</h1>
+          <div>
+            <input
+              type="radio"
+              checked={isPublished == 1}
+              name="Published"
+              value={1}
+              id="published"
+              onChange={(e) => setIsPublished(e.target.value)}
+            />
+            <label htmlFor="published"> Published</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              checked={isPublished == 0}
+              name="Save as draft"
+              value={0}
+              id="save-as"
+              onChange={(e) => setIsPublished(e.target.value)}
+            />
+            <label htmlFor="save-as"> Save as draft</label>
+          </div>
+
+          <label htmlFor="file">
+            <b>Upload Image</b>{" "}
+          </label>
+          {/* Need check if there is  an img or not -- if we come to edit or to create new post  */}
+          <input
+            type="file"
+            id="file"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+        </div>
+        <div className="item">
+          <h1>Category</h1>
+          <div>
+            <input
+              type="radio"
+              checked={cat.toLowerCase() === "art"}
+              name="cat"
+              value="Art"
+              id="art"
+              onChange={(e) => setCat(e.target.value)}
+            />
+            <label htmlFor="art">Art</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              checked={cat.toLowerCase() === "food"}
+              name="cat"
+              value="Food"
+              id="Food"
+              onChange={(e) => setCat(e.target.value)}
+            />
+            <label htmlFor="Food">Food</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              checked={cat.toLowerCase() === "technology"}
+              name="cat"
+              value="Technology"
+              id="Technology"
+              onChange={(e) => setCat(e.target.value)}
+            />
+            <label htmlFor="Technology">Technology</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              checked={cat.toLowerCase() === "science"}
+              name="cat"
+              value="Science"
+              id="Science"
+              onChange={(e) => setCat(e.target.value)}
+            />
+            <label htmlFor="Science">Science</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              checked={cat.toLowerCase() === "sport"}
+              name="cat"
+              value="Sport"
+              id="Sport"
+              onChange={(e) => setCat(e.target.value)}
+            />
+            <label htmlFor="Sport">Sport</label>
+          </div>
+        </div>
+        <div className="buttons">
+          <button onClick={handleClick}>Publish</button>
+        </div>
+      </div>
+    </div>
+  );
 }
